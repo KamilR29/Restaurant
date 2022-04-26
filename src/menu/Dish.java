1c152ffff3372c3a32d1e98577de6bd770799c0a
Kamil <kamil.raczkowski@onet.pl>
2022-04-26 22:11:34 +0200

    Add Dish and Order class

---
+++ src/menu/Dish.java
@@ -0,0 +1,16 @@
+package menu;
+
+public class Dish {
+
+    private int number;
+    private String name;
+    private String description;
+    private int price;
+
+    public Dish(int number, String name, String description, int price) {
+        this.number = number;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
---
+++ src/order/Order.java
@@ -0,0 +1,28 @@
+package order;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Order {
+
+    private int number;
+
+    public Order() {
+        this.number = setNumber();
+    }
+
+    public int setNumber(){
+
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HHmm");
+        String time =  dateFormat.format(date);
+        int code = Integer.parseInt(time);
+        return code;
+    }
+    public void showNumber(){
+        System.out.println(number);
+    }
+
+
+}
